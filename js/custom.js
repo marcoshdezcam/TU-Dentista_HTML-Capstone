e51d09379a27cf001360ec3b4b9a2b7d334d4900
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-06-02 21:31:21 -0500

    FIX: Layout on XL
    Change container-fluid to container-lg to avoid big containers & images

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,16 @@
 var prevScrollpos = window.pageYOffset;
+var screenWidth = screen.width;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar--main").style.top = "0";
-  } else {
-    document.getElementById("navbar--main").style.top = "-110px";
-    document.getElementById("results--page").style.top = "-110px";
-
+  if (screenWidth < 576) {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar--main").style.top = "0";
+      document.getElementById("results--page").style.marginTop = "25vh";
+    } else {
+      document.getElementById("navbar--main").style.top = "-130px";
+      document.getElementById("results--page").style.marginTop = "130px";
+  
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
 } 
